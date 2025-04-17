efefae17a0f7929548092705898218be8b948c6e
Tim <info@timluginbuehl.net>
2025-04-17 16:04:15 +0200

    fixed package.json

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  base: process.env.NETLIFY ? "/" : undefined,
+  build: {
+    outDir: "dist"
+  },
   plugins: [
     VitePWA({
       registerType: "autoUpdate",
